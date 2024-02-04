fa6eb1bbc45fab2116a2b2503ca496087a381f62
Anand.C <anandchannazhi01@gmail.com>
2024-02-04 19:47:20 +0530

    Alerts.jsx file content added

---
+++ src/components/Alerts.jsx
@@ -0,0 +1,13 @@
+// src/components/Alerts.jsx
+import React from 'react';
+
+const Alerts = () => {
+  return (
+    <div>
+      <h2>Alerts Section</h2>
+      {/* Add alerts-related content and logic here */}
+    </div>
+  );
+};
+
+export default Alerts;
