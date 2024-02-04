d0bc050897723d8ed810144d52b4c735edebbc9b
Anand.C <anandchannazhi01@gmail.com>
2024-02-04 19:43:16 +0530

    Budget.jsx file content added

---
+++ src/components/Budget.jsx
@@ -0,0 +1,13 @@
+// src/components/Budget.jsx
+import React from 'react';
+
+const Budget = () => {
+  return (
+    <div>
+      <h2>Budget Section</h2>
+      {/* Add budget-related content and logic here */}
+    </div>
+  );
+};
+
+export default Budget;
