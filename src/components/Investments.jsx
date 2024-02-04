bf3ad582dc344f9adbf9f8366adc5b59a44e472c
Anand.C <anandchannazhi01@gmail.com>
2024-02-04 19:45:28 +0530

    Investments.jsx file content added

---
+++ src/components/Investments.jsx
@@ -0,0 +1,13 @@
+// src/components/Investments.jsx
+import React from 'react';
+
+const Investments = () => {
+  return (
+    <div>
+      <h2>Investments Section</h2>
+      {/* Add investment-related content and logic here */}
+    </div>
+  );
+};
+
+export default Investments;
