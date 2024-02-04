cc1f012e9aa00adcb0f8e42309e6c4aeb173a669
Anand.C <anandchannazhi01@gmail.com>
2024-02-04 19:46:34 +0530

    reports.jsx file content added

---
+++ src/components/Reports.jsx
@@ -0,0 +1,13 @@
+// src/components/Reports.jsx
+import React from 'react';
+
+const Reports = () => {
+  return (
+    <div>
+      <h2>Reports Section</h2>
+      {/* Add reports-related content and logic here */}
+    </div>
+  );
+};
+
+export default Reports;
